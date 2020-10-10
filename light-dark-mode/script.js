5823c3b8fb130cec0188d735f4931c8a2dcb1ca4
jinyoung <sounddribble@gmail.com>
2020-10-10 14:49:41 +0900

    ✨Guide Page added

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,9 +1,7 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById("nav");
 const toggleIcon = document.getElementById("toggle-icon");
-const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
 const textbox = document.getElementById("text-box");
 const DARK_THEME = 'dark';
 const LIGHT_THEME = 'light';
@@ -27,9 +25,7 @@ function lightMode() {
 }
 
 function imageMode(color) {
-  image1.src = `img/undraw_Devices_${color}.svg`;
   image2.src = `img/undraw_Detailed_information_${color}.svg`;
-  image3.src = `img/undraw_Landing_page_${color}.svg`;
 }
 
 function switchTheme(event) {
